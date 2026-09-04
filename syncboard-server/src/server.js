b100f7ef37894b1f8fd16b9e2ca2150d675c1357
Panchalee Hewage <panchaleehewage50@gmail.com>
2026-09-04 19:47:47 +0530

    fix: execute database connection in server entry point before listening for requests

--- syncboard-server/src/server.js
+++ syncboard-server/src/server.js
@@ -2,6 +2,8 @@ import app from './app.js';
 import { config } from './config.js';
 import { connectDB } from './db/connect.js';
 
+await connectDB();
+
 app.listen(config.port, () => {
   console.log(`Syncboard Server running in ${config.env} mode on port ${config.port}...`);
 });
